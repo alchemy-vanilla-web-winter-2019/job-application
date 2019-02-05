09345708dda2e77b706a4bdadd1f7b366339a55b
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-05 09:00:58 -0800

    got the buttons to work, and added css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const form = document.getElementById('form');
 const mathProwess = document.getElementById('math-prowess');
 const mathProwessLevel = document.getElementById('how-well-do-you-code');
 
+const story = document.getElementById('origin-story');
+
 mathProwess.addEventListener('change', function(event) {
     event.preventDefault();
     mathProwessLevel.textContent = mathProwess.value;
@@ -17,13 +19,22 @@ form.addEventListener('submit', function(event) {
     const homeWorld = form.elements.homeWorld.value;
     const homeCity = form.elements.homeCity.value;
 
-    
+    const religion = form.elements.religions;
+    const followerOf = [];
+    for(let i = 0; i < religion.length; i++) {
+        const choosenReligion = religion[i];
+        if(choosenReligion.checked) {
+            followerOf[i] = choosenReligion.value;
+        }
+    }
     
     const ensignApplication = {
         name: callSign,
         loyalty: loyalty,
         placeOfOrigin: [homeWorld, homeCity],
-        computingCompitence: mathProwess.value
+        computingCompitence: mathProwess.value,
+        religion: followerOf,
+        originStory: story.value
     };
     console.log(ensignApplication);
 });
