bab0320d0bc6c99136c5698b21b910002ab7c1f8
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-04 17:36:03 -0800

    working checkboxes

--- src/apply.js
+++ src/apply.js
@@ -1,1 +1,21 @@
-console.log('hi');
+const applicationFormNode = document.getElementById('application-form');
+// const nameInputNode = document.getElementById('name-input');
+
+applicationFormNode.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const skillsArray = [];
+    for(let i = 0; i < applicationFormNode.skills.length; i++) {
+        if(applicationFormNode.skills[i].checked) {
+            skillsArray.push(applicationFormNode.skills[i].value)
+        }   
+    }
+
+    const newApplication = {
+        name: applicationFormNode.name.value,
+        assassination: applicationFormNode.assassination.value,
+        clothes: applicationFormNode.clothes.value,
+        skills: skillsArray
+    };
+    console.log('New App', newApplication);
+});
