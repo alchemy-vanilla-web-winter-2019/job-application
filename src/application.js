7d38653da49e9848710338e80c85c583e326b68b
Cheri Portman <cheriportman@hotmail.com>
2019-02-04 17:51:32 -0800

    working on radio button data transfer

--- src/application.js
+++ src/application.js
@@ -1,11 +1,18 @@
 const application = document.getElementById('application');
-const name = document.getElementById('name');
+const heroName = document.getElementById('hero-name');
 const alias = document.getElementById('alias');
+const profession = document.getElementsByName('profession');
 
 
 const applicant = {};
 
 application.addEventListener('submit', function(event) {
     event.preventDefault();
-    
-})
+
+    applicant.name = heroName.value;
+    applicant.alias = alias.value;
+    applicant.profession = profession;
+
+console.log(applicant);
+
+});
