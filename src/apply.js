94c349fb3b454e12f551588922df81ead8bed903
Parker Helland <hellandp@gmail.com>
2019-02-05 09:23:04 -0800

    got checkboxes to work in the object and store values

--- src/apply.js
+++ src/apply.js
@@ -3,13 +3,25 @@ const nameInputNode = document.getElementById('name-input');
 const digitsInputNode = document.getElementById('digits-input');
 const emailInputNode = document.getElementById('email-input');
 
+
 appFormNode.addEventListener('submit', function(event) {
     event.preventDefault();
     
+    //console.log(appFormNode.checkboxes);
+    let checkBoxesArray = [];
+    for(let i = 0; i < appFormNode.checkboxes.length; i++) {
+        let selectedCheck = appFormNode.checkboxes[i];
+        if(selectedCheck.checked) {
+            checkBoxesArray.push(selectedCheck.value);
+        }
+    } //console.log('success', checkBoxesArray);
+
     let formSubmittal = {
         name: nameInputNode.value,
         digits: digitsInputNode.value,
         email: emailInputNode.value,
+        talkboss: checkBoxesArray
+
     };
     console.log('I did it', formSubmittal);
 });
