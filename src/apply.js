07b92ee52abacaaaa566bb0ec1946368b68aad12
Peter Kotowski <bighoopee@gmail.com>
2019-02-04 20:55:02 -0800

    added some style

--- src/apply.js
+++ src/apply.js
@@ -2,9 +2,55 @@ const userInputNode = document.getElementById('user-input');
 const nameNode = document.getElementById('name');
 const cityNode = document.getElementById('city');
 const phoneNumberNode = document.getElementById('phone-number');
-// const yesRealityNode = document.getElementById('yes-reality');
-// const noRealityNode = document.getElementById('no-reality');
-// const selfDescription = document.getElementById('self-description');
+const yesEscapeNode = document.getElementById('yes-escape');
+const noEscapeNode = document.getElementById('no-escape');
+const boredomNode = document.getElementById('boredom');
+const delusionalNode = document.getElementById('delusional');
+const bowieNode = document.getElementById('bowie');
+const selfDescription = document.getElementById('self-description');
+
+
+yesEscapeNode.addEventListener('change', function() {
+    if(yesEscapeNode.checked) {
+        const hidden = document.querySelectorAll('.escape-reason');
+        for(let index = 0; index < hidden.length; index++) {
+            hidden[index].classList.remove('hidden');
+        }
+        boredomNode.disabled = false;
+        delusionalNode.disabled = false;
+        bowieNode.disabled = false;
+        
+        
+        
+        
+    }
+});
+
+noEscapeNode.addEventListener('change', function() {
+    if(noEscapeNode.checked) {
+        const hidden = document.querySelectorAll('.escape-reason');
+        for(let index = 0; index < hidden.length; index++) {
+            hidden[index].classList.add('hidden');
+        }
+        boredomNode.disabled = false;
+        delusionalNode.disabled = false;
+        bowieNode.disabled = false;
+
+        boredomNode.disabled = true;
+        delusionalNode.disabled = true;
+        bowieNode.disabled = true;
+
+        
+        
+        
+        
+        
+    }
+});
+
+
+
+
 
 
 
@@ -15,9 +61,10 @@ userInputNode.addEventListener('submit', function(event) {
         name: nameNode.value,
         city: cityNode.value,
         phone: phoneNumberNode.value,
-        // escapeReason: escapeSelect,
+        describeSelf: selfDescription.value,
+    
         
         
     };
-    console.log(applicant);
+    
 });
