66a78a8e68bc838f38fe7a7d06a7ec5c10321937
Leslie Shea <leslieyshea@gmail.com>
2019-02-05 15:23:45 -0800

    added applicant-details js page to pull info from json back into js

--- src/applicant-details.js
+++ src/applicant-details.js
@@ -1,2 +1,28 @@
 const applicantJSON = window.localStorage.getItem('applicant');
 const applicant = JSON.parse(applicantJSON);
+
+const firstNameNode = document.getElementById('first-name');
+const lastNameNode = document.getElementById('last-name');
+const zipCodeNode = document.getElementById('zip-code');
+const emailNode = document.getElementById('email');
+const lengthTimeNode = document.getElementById('length');
+const hoursNode = document.getElementById('hours');
+const genreNode = document.getElementById('genre');
+
+
+firstNameNode.textContent = applicant.firstName;
+lastNameNode.textContent = applicant.lastName;
+zipCodeNode.textContent = applicant.zipCode;
+emailNode.textContent = applicant.email;
+lengthTimeNode.textContent = applicant.lengthTime;
+hoursNode.textContent = applicant.hours;
+
+if(applicant.genres.length === 0) {
+    genreNode.textContent = 'none';
+}
+else {
+    for(let index = 0; index < applicant.genres.length - 1; index++) {
+        genreNode.textContent += applicant.genres[index] + ', ';
+    }
+    genreNode.textContent += applicant.genres[applicant.genres.length - 1];
+}
