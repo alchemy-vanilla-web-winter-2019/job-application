98a8c0983027e0311c4bd5c50264ebcaf8c136aa
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-04 22:33:41 -0800

    more HTML and JS

--- src/apply.js
+++ src/apply.js
@@ -1,19 +1,49 @@
-const formNode = document.getElementById('form-details');
+const formNode = document.getElementById('detail-form');
 const nameNode = document.getElementById('name');
-const yesNode = document.getElementById('yes');
-const noNode = document.getElementById('no');
+const marriedNode = document.getElementById('married');
+const singleNode = document.getElementById('single');
 const statusNode = document.getElementsByName('status');
-const 
+const employmentNode = document.getElementsByName('employment');
+const yesEmployedNode = document.getElementById('yes');
+const noEmployedNode = document.getElementById('no');
+const salaryNode = document.getElementById('salary');
 
+marriedNode.addEventListener('change', function() {
+    if(marriedNode.checked) {
+        statusNode.value = 'married';
+    }
+});
+
+singleNode.addEventListener('change', function() {
+    if(singleNode.checked) {
+        statusNode.value = 'single';
+    }
+});
+
+yesEmployedNode.addEventListener('change', function() {
+    if(yesEmployedNode.checked) {
+        salaryNode.disabled = false;
+        employmentNode.value = "employed";
+    }
+});
+
+noEmployedNode.addEventListener('change', function() {
+    if(noEmployedNode.checked) {
+        salaryNode.disabled = true;
+        employmentNode.value = 'not employed';
+    }
+});
 
 
 formNode.addEventListener('submit', function(event) {
     event.preventDefault();
-    
+    const habits = document.getElementsByName('habits[]')
+    console.log(habits);
     const applicant = {
         name: nameNode.value,
         status: statusNode.value,
+        employment: employmentNode.value
+    
     };
-});
-
 console.log(applicant);
+});
