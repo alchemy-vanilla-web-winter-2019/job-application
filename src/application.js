e142d980cbd04e8c0aba054e444adf499f3c5100
Cheri Portman <cheriportman@hotmail.com>
2019-02-04 20:34:42 -0800

    stuck.

--- src/application.js
+++ src/application.js
@@ -2,7 +2,9 @@ const application = document.getElementById('application');
 const heroName = document.getElementById('hero-name');
 const alias = document.getElementById('alias');
 const profession = document.getElementsByName('profession');
+const combatNodeList = document.getElementsByName('combat').checked;
 
+console.log('combatNodeList', combatNodeList);
 
 const applicant = {};
 
@@ -11,7 +13,18 @@ application.addEventListener('submit', function(event) {
 
     applicant.name = heroName.value;
     applicant.alias = alias.value;
-    applicant.profession = profession;
+    applicant.profession = application.profession.value;
+
+    // const combatStyles = [];
+
+    // for(let i = 0; i < combatNodeList.length; i++) {
+    //     const style = combatNodeList[i];
+    //     if(style.checked) {
+    //         console.log('for loop', style[i]);
+            // combatStyles.push(style.value);
+        // }
+        // console.log(style);
+    // }
 
 console.log(applicant);
 
