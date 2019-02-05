42e15022719f71d71b38558611429d62f1cfb0b9
Ben Parisot <me@benparisot.com>
2019-02-04 17:40:45 -0800

    get skills radio buttons to be read by js

--- src/apply.js
+++ src/apply.js
@@ -1,13 +1,39 @@
 const applicationFormNode = document.getElementById('application-form');
 const nameNode = document.getElementById('name');
+const cityNode = document.getElementById('city');
+const computersNode = document.getElementById('computers');
+const artsNode = document.getElementById('arts');
+const musicNode = document.getElementById('music');
+const engineeringNode = document.getElementById('engineering');
+const domesticNode = document.getElementById('domestic');
 
-console.log(applicationFormNode);
-console.log(nameNode);
+const strongestSkill = [];
+
+// console.log(applicationFormNode);
+// console.log(nameNode);
+
+//get skills
+
+
 
 applicationFormNode.addEventListener('submit', function() {
     event.preventDefault();
+
+    const skills = document.getElementsByName('skills');
+
+
+    for(let index = 0; index < skills.length; index++) {
+        if(skills[index].checked) {
+            console.log(skills[index].value);
+        }
+    }
+
+    // console.log(skills);
+
+
     const applicant = {
         name: nameNode.value
+        // skill: 
     };
     
     console.log(applicant);
