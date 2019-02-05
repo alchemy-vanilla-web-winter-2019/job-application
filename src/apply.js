94e4447c4426679f4038127738f279234a3ddd02
Leslie Shea <leslieyshea@gmail.com>
2019-02-04 17:41:25 -0800

    added html structure for page, some css styling and declared nodes in js

---
+++ src/apply.js
@@ -0,0 +1,5 @@
+const formNode = document.getElementById('form');
+const firstNameNode = document.getElementById('first-name');
+const lastNameNode = document.getElementById('last-name');
+const zipCodeNode = document.getElementById('zip-code');
+const emailNode = document.getElementById('email');
