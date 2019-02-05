8699c51ad6995642b6b46242645d00c7506ccc02
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-05 15:58:09 -0800

    finished

--- src/applicant-details.js
+++ src/applicant-details.js
@@ -1,12 +1,8 @@
 const getPackage = window.localStorage.getItem('job-applicant');
 const wetPackage = JSON.parse(getPackage);
-const returnFormNode = document.getElementById('return-form');
-const returnComfortNode = document.getElementById('return-comfort');
-const formItems = ['name', 'phone', 'comfort', 'burial'];
 const formValue = [wetPackage.name, wetPackage.phone, wetPackage.comfort, wetPackage.burial];
 
 for(let index = 0; index < 4; index++) {
     const form = document.getElementsByTagName('dd');
     form[index].textContent = formValue[index];
-    console.log(wetPackage);
 }
