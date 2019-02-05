d805bb89e38de50ea8a82ee26ea39ee95240a715
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-04 21:32:44 -0700

    for loop for checkboxes working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const spaceForm = document.getElementById('application');
 const userName = document.getElementById('name');
 const spaceship = document.getElementById('spaceship');
-const clausterphobic = document.getElementById('clausterphobic');
-const clausterphobicDisplay = document.getElementById('slider-display');
+const claustrophobic = document.getElementById('claustrophobic');
+const claustrophobicDisplay = document.getElementById('slider-display');
 
 
-clausterphobic.addEventListener('change', function() {
+claustrophobic.addEventListener('change', function() {
+    claustrophobicDisplay.textContent = claustrophobic.value;
+
+});
+
+const chosenGear = [];
+
+spaceForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    for(let index = 0; index < spaceForm.spacegear.length; index++) {
+        const gear = spaceForm.spacegear[index];
+        if(gear.checked) {
+            chosenGear[index] = gear.value;
+        }
+    }
+
+});
 
 
-)};
