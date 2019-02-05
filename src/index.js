244e842430f37dfc6209b0a961c8154af149e2a4
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-04 18:46:44 -0700

    scaffled, created HTML application page, created base consts in JS, beginning event listener for slider

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const spaceForm = document.getElementById('application');
+const userName = document.getElementById('name');
+const spaceship = document.getElementById('spaceship');
+const clausterphobic = document.getElementById('clausterphobic');
+const clausterphobicDisplay = document.getElementById('slider-display');
+
+
+clausterphobic.addEventListener('change', function() {
+
+
+)};
