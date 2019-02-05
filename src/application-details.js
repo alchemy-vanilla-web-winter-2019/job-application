100baf824316e5c6222f88f18141728255f88d54
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-05 15:18:22 -0800

    more work with JSON

--- src/application-details.js
+++ src/application-details.js
@@ -8,7 +8,14 @@ const habitListNode = document.getElementById('habits');
 
 nameNode.textContent = applicant.name;
 statusNode.textContent = applicant.status;
-employmentNode.textContent = applicant.status;
-salaryNode.textContent = applicant.salary;
-habitListNode.textContent = applicant.habitList;
+employmentNode.textContent = applicant.employment;
+habitListNode.textContent = applicant.habits;
+salaryNode.textConent = applicant.salary;
 
+// if(employmentNode.value = 'employed') {
+//     salaryNode.classList.add('hidden-2');  
+// }
+// else {
+//     salaryNode.classList.remove('hidden-2');
+//     salaryNode.textContent = applicant.salary;
+// }
