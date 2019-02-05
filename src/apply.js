b1405329dff285b232c9b15efb685985d76d0e0f
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-04 17:47:44 -0800

    added more input elements and appended them to the form submission js, basic styling

--- src/apply.js
+++ src/apply.js
@@ -1,3 +1,28 @@
 const guildApplication = document.getElementById('guild-application');
 
-console.log(guildApplication);
+//Idea!
+//Use guildApplication.elements to initiate a for loop 
+//which collects all nodes with name attribute
+
+guildApplication.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const inputRoles = [];
+
+    for(let i = 0; i < guildApplication.roles.length; i++) {
+        const role = guildApplication.roles[i];
+        if(role.checked) {
+            inputRoles.push(role.value);
+        }
+    }
+    const application = {
+        name: guildApplication.name.value,
+        experience: guildApplication.experience.value,
+        weapon: guildApplication.weapon.value,
+        roles: inputRoles,
+        goodness: guildApplication.goodness.value,
+        lawfulness: guildApplication.lawfulness.value,
+        
+    };
+
+});
+
