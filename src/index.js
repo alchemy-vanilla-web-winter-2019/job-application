85b7ab50dee666b9202be873ac9a3039a506bec8
Alexandre Muniz <aom@puzzlezapper.com>
2019-02-04 20:21:39 -0800

    Added checkboxes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const faireApplication = document.getElementById('faire-application');
 
-
 faireApplication.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const talentNames = [];
+    for(let i = 0; i < faireApplication.talent.length; i++) {
+        const name = faireApplication.talent[i];
+        if(name.checked) {
+            talentNames.push(name.value);
+        }
+    }
 
+    const application = {
+        name: faireApplication.name.value,
+        quest: faireApplication.quest.value,
+        color: faireApplication.color.value,
+        talents: talentNames
+    };
+
+    console.log(application);
 
 });
