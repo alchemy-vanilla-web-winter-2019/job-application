355843728256f475e09675c35c954c5bedc2b14f
Leslie Shea <leslieyshea@gmail.com>
2019-02-04 17:46:49 -0800

    added event listener for form node and added applicant object with some properties

--- src/apply.js
+++ src/apply.js
@@ -3,3 +3,21 @@ const firstNameNode = document.getElementById('first-name');
 const lastNameNode = document.getElementById('last-name');
 const zipCodeNode = document.getElementById('zip-code');
 const emailNode = document.getElementById('email');
+
+
+
+
+
+formNode.addEventListener('submit', function(event) {
+    event.defaultPrevented();
+
+    const applicant = {
+        firstName: firstNameNode.value,
+        lastName: lastNameNode.value,
+        zipCode: zipCodeNode.value,
+        email: emailNode.value
+
+    };
+    console.log(applicant);
+
+});
