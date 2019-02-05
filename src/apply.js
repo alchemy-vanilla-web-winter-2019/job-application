68c3b3dd4f64dfb7f2663899b621132793c80cf8
Parker Helland <hellandp@gmail.com>
2019-02-04 17:36:45 -0800

    added contact details legend and connected apply.js object upon event of clicking

--- src/apply.js
+++ src/apply.js
@@ -1,6 +1,14 @@
 const appFormNode = document.getElementById('app-form');
+const nameInputNode = document.getElementById('name-input');
+const digitsInputNode = document.getElementById('digits-input');
+const emailInputNode = document.getElementById('email-input');
 
 appFormNode.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log('clicked');
+    let formSubmittal = {
+        name: nameInputNode.value,
+        digits: digitsInputNode.value,
+        email: emailInputNode.value
+    };
+    console.log('I did it', formSubmittal);
 });
