5a142e78ad3ef49463c582f6d1b3274de0b60dcd
Emily Creamer <emily.creamer@icloud.com>
2019-02-05 10:28:41 -0800

    added js for slider and movie types

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 const jobApp = document.getElementById('job-app');
 const appName = document.getElementById('name');
-const positions = document.getElementById('positions');
+const jobPosition = document.getElementById('positions');
 const popcorn = document.getElementById('popcorn');
 const popcornSpan = document.getElementById('popcorn-span');
+
+
+
+popcorn.addEventListener('change', function() {
+    event.preventDefault();
+    popcornSpan.textContent = popcorn.value;
+
+});
 
 
 jobApp.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const romComClick = [];
+    for(let index = 0; index < jobApp.romcom.length; index++){
+        const romComAnswer = jobApp.romcom[index];
+        if(romComAnswer.checked) {
+            romComClick[index] = romComAnswer.value;
+        }
+    }
+
     const filmNames = [];
     for(let index = 0; index < jobApp.films.length; index++){
-        const name = jobApp.films[index];
-        if(name.checked) {
-            filmNames[index] = name.value;
+        const filmName = jobApp.films[index];
+        if(filmName.checked) {
+            filmNames[index] = filmName.value;
         }
     }
 });
+
+
