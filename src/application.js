9332d3fc0de84f41544bc19227b164502fd06a82
Cheri Portman <cheriportman@hotmail.com>
2019-02-04 21:07:32 -0800

    I did it. Checkboxes values are getting pushed into the object properly. That hurt!

--- src/application.js
+++ src/application.js
@@ -2,30 +2,32 @@ const application = document.getElementById('application');
 const heroName = document.getElementById('hero-name');
 const alias = document.getElementById('alias');
 const profession = document.getElementsByName('profession');
-const combatNodeList = document.getElementsByName('combat').checked;
+const combatNodeList = document.getElementsByName('combat');
 
-console.log('combatNodeList', combatNodeList);
+// console.log('combatNodeList', combatNodeList);
 
 const applicant = {};
 
 application.addEventListener('submit', function(event) {
     event.preventDefault();
+    const combatStyles = [];
 
     applicant.name = heroName.value;
     applicant.alias = alias.value;
     applicant.profession = application.profession.value;
+    applicant.combat = combatStyles;
 
-    // const combatStyles = [];
 
-    // for(let i = 0; i < combatNodeList.length; i++) {
-    //     const style = combatNodeList[i];
-    //     if(style.checked) {
-    //         console.log('for loop', style[i]);
-            // combatStyles.push(style.value);
-        // }
-        // console.log(style);
-    // }
+    for(let i = 0; i < combatNodeList.length; i++) {
+        const style = combatNodeList[i];
+        if(style.checked) {
+            // console.log('style checked', style);
+            combatStyles.push(style.value);
+        }
+        console.log('combatStyles array', combatStyles);
+        // console.log('style', style);
+    }
 
-console.log(applicant);
+console.log('applicant', applicant);
 
 });
