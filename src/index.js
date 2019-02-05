cd0cc6ea11bf77fb235a05ee5eb17ad9f7094c3f
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-04 21:39:35 -0700

    added second event listener for form submit, stored all values in an object

--- src/index.js
+++ src/index.js
@@ -14,13 +14,21 @@ const chosenGear = [];
 
 spaceForm.addEventListener('submit', function(event) {
     event.preventDefault();
+    console.log(userName.value);
     for(let index = 0; index < spaceForm.spacegear.length; index++) {
         const gear = spaceForm.spacegear[index];
         if(gear.checked) {
             chosenGear[index] = gear.value;
         }
     }
-
+    const userData = {
+        name: userName.value,
+        spaceship: spaceship.value, 
+        claustrophobic: claustrophobic.value,
+        spacegear: chosenGear
+    };
+    console.log(userData);
 });
+
 
 
