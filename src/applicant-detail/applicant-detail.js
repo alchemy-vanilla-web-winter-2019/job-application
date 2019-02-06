aaf35f66a82a57e2a6a41fa130a4073707e3f544
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-05 16:57:34 -0800

    added some styling and secrets

--- src/applicant-detail/applicant-detail.js
+++ src/applicant-detail/applicant-detail.js
@@ -4,12 +4,15 @@ const clothesNode = document.getElementById('clothes');
 const skillsNode = document.getElementById('skills');
 const snuckNode = document.getElementById('snuck');
 
+const snuckTitleNode = document.getElementById('snuck-title');
+
 const applicantSerialized = window.localStorage.getItem('newApplication');
 const applicantParsed = JSON.parse(applicantSerialized);
-console.log(applicantParsed);
 
 nameNode.textContent = applicantParsed.name;
 assassinationNode.textContent = applicantParsed.assassination;
 clothesNode.textContent = applicantParsed.clothes;
 skillsNode.textContent = applicantParsed.skills.join(', ');
 snuckNode.textContent = applicantParsed.snuck;
+
+snuckTitleNode.innerHTML = `Last time ${applicantParsed.name} snuck up on someone`;
