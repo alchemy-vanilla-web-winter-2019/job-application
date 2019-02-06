b9557bdbf5960fb8692882157efb7e6ce33287c0
Alexandre Muniz <aom@puzzlezapper.com>
2019-02-05 16:16:52 -0800

    hooked application details page up to the JSON from localStorage.

--- src/application-details.js
+++ src/application-details.js
@@ -1,3 +1,14 @@
+const nameEl = document.getElementById('name');
+const questEl = document.getElementById('quest');
+const colorEl = document.getElementById('color');
+const talentsEl = document.getElementById('talents');
+
+
 const jsonObject = window.localStorage.getItem('application');
 const hydratedJson = JSON.parse(jsonObject);
 console.log(hydratedJson);
+
+nameEl.textContent = hydratedJson.name;
+questEl.textContent = hydratedJson.quest;
+colorEl.textContent = hydratedJson.color;
+talentsEl.textContent = hydratedJson.talents.join(', ');
