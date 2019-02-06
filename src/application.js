23db2bb53ed5a0c415b383dff3190d3bb2b92765
Cheri Portman <cheriportman@hotmail.com>
2019-02-05 16:58:49 -0800

    have space set up for JSON insertion...

--- src/application.js
+++ src/application.js
@@ -28,4 +28,12 @@ application.addEventListener('submit', function(event) {
         console.log('combatStyles array', combatStyles);
     }
     console.log('applicant', applicant);
+
+    //get the object into a variable
+    const serialized = JSON.stringify(applicant);
+    // set the object into local storage ('keyname', value)
+    window.localStorage.setItem('applicant', serialized);
+    window.location = 'thanks.html';
 });
+
+
