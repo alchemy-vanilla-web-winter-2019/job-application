495eeddb660fec7afc8ce907ffb18c1baedbc760
mcnadams <mcneilbo@oregonstate.edu>
2019-02-06 14:56:24 -0800

    add column to table displaying cuddle types from array

--- src/applicants.js
+++ src/applicants.js
@@ -9,17 +9,31 @@ else {
     window.location = './index.html';
 }
 
+console.log(applicants);
+
 for(let i = 0; i < applicants.length; i++) {
     const applicant = applicants[i];
 
     const tr = document.createElement('tr');
     const name = document.createElement('td');
     const city = document.createElement('td');
+    const cuddleTypes = document.createElement('td');
 
     name.textContent = applicant.name;
     city.textContent = applicant.city;
 
+    if(applicant.cuddles.length === 0) {
+        cuddleTypes.textContent = 'none';
+    }
+    else {
+        for(let i = 0; i < applicant.cuddles.length - 1; i++) {
+            cuddleTypes.textContent += applicant.cuddles[i] + ', ';
+        }
+        cuddleTypes.textContent += applicant.cuddles[applicant.cuddles.length - 1];
+    }
+
     tr.appendChild(name);
     tr.appendChild(city);
+    tr.appendChild(cuddleTypes);
     tbody.appendChild(tr);
 }
