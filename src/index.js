707054ad81c8b3f4ce8346f7c2b62d3607765d39
Elizabeth Keppler <lizzykeppler@gmail.com>
2019-02-05 16:27:08 -0800

    updated js

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const jobApp = document.getElementById('job-app');
+const appName = document.getElementById('name');
+const jobPosition = document.getElementById('positions');
+const popcorn = document.getElementById('popcorn');
+const popcornSpan = document.getElementById('popcorn-span'); 
+
+popcorn.addEventListener('change', function() {
+    event.preventDefault();
+    popcornSpan.textContent = popcorn.value; 
+
+});
+
+jobApp.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const romComClick = [];
+    for(let index = 0; index < jobApp.romcom.length; index++) {
+        const romComAnswer = jobApp.romcom[index];
+        if(romComAnswer.checked) {
+            romComClick[index] = romComAnswer.value;
+        }
+    }
+
+    const filmNames = [];
+    for(let index = 0; index < jobApp.films.length; index++) {
+        const filmName = jobApp.films[index];
+        if(filmName.checked) {
+            filmNames[index] = filmName.value;
+        }
+    }
+
+    const applicant = {
+        name: appName.value,
+        position: jobPosition.value,
+        popcorn: popcorn.value,
+        romCom: romComClick.value,
+        filmNames: filmNames.value
+    };
+    console.log(applicant);
+})
