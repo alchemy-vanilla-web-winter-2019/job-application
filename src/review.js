72fdcebfe673168a9e934e52e96569c72e5cae17
Tommy Tran <tranttommy95@gmail.com>
2019-02-06 09:52:24 -0800

    fixed review.js

--- src/review.js
+++ src/review.js
@@ -9,7 +9,6 @@ const applicant = JSON.parse(json);
 
 document.getElementById('name').textContent = applicant.name;
 document.getElementById('city-state').textContent = applicant.address.city + ', ' + applicant.address.state;
-document.getElementById('state').textContent = applicant.address.state;
 document.getElementById('steak').textContent = applicant.steak;
 document.getElementById('curliness').textContent = applicant.curliness;
 document.getElementById('salad').textContent = applicant.salad.join(', ');
