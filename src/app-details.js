3ef0f61ae738c9fcc13113de91a357618cc38414
Elizabeth Keppler <lizzykeppler@gmail.com>
2019-02-05 17:12:37 -0800

    finished without styling

---
+++ src/app-details.js
@@ -0,0 +1,14 @@
+const name = document.getElementById('name');
+const position = document.getElementById('position');
+const popcorn = document.getElementById('popcorn');
+const romcom = document.getElementById('romcom');
+const films = document.getElementById('films');
+
+const json = window.localStorage.getItem('applicant');
+const filmApplicant = JSON.parse(json);
+
+name.textContent = filmApplicant.name;
+position.textContent = filmApplicant.position; 
+popcorn.textContent = filmApplicant.popcorn;
+romcom.textContent = filmApplicant.romCom.join(' ');
+films.textContent = filmApplicant.films.join(' ');
