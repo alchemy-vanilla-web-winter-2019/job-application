61f1841be6836de3cc7278f752c97b41427b3a96
Heather <unphurlng@gmail.com>
2019-02-05 17:46:51 -0800

    applicant-detail js

--- src/applicant-detail/applicant-detail.js
+++ src/applicant-detail/applicant-detail.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const gender = document.getElementById('gender');
+
+const jsonObject = window.localStorage.getItem('applicant');
+const hydratedApplicant = JSON.parse(jsonObject);
+
+name.textContent = hydratedApplicant.name;
+email.textContent = hydratedApplicant.email;
+phone.textContent = hydratedApplicant.phone;
+gender.textContent = hydratedApplicant.gender;
