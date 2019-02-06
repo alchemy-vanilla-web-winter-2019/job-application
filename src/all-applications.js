aad7d217f92bc3dcc58e6a27ee450a48f43b5369
Ben Parisot <me@benparisot.com>
2019-02-06 15:48:04 -0800

    get table of all applicants to display on all applications page

--- src/all-applications.js
+++ src/all-applications.js
@@ -1,7 +1,39 @@
 const applicantJSON = window.localStorage.getItem('applicant');
-if(!applicantJSON)
+if(!applicantJSON) {
+    document.location = '../index.html';
+}
 
-let applicant = JSON.parse(applicantJSON);
+const applicants = JSON.parse(applicantJSON);
 
+const applicantsNode = document.getElementById('applicants');
 
-console.log(applicant);
+for(let index = 0; index < applicants.length; index++) {
+    console.log('hey');
+    const applicant = applicants[index];
+
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    const city = document.createElement('td');
+    const skill = document.createElement('td');
+    const philosophy = document.createElement('td');
+    const morality = document.createElement('td');
+    const salary = document.createElement('td');
+
+    name.textContent = applicant.name;
+    city.textContent = applicant.city;
+    skill.textContent = applicant.skill;
+    philosophy.textContent = applicant.philosophy;
+    morality.textContent = applicant.morality;
+    salary.textContent = applicant.salary;
+    
+    row.appendChild(name);
+    row.appendChild(city);
+    row.appendChild(skill);
+    row.appendChild(philosophy);
+    row.appendChild(morality);
+    row.appendChild(salary);
+
+    applicantsNode.appendChild(row);
+
+}
+
