bd08f8f98c8523d4ff3d061abd07ecdcf899ec54
Alexandre Muniz <aom@puzzlezapper.com>
2019-02-05 16:58:01 -0800

    Added loyalty to the JSON, (and had it take it out of the JSON.)

--- src/application-details.js
+++ src/application-details.js
@@ -2,6 +2,8 @@ const nameEl = document.getElementById('name');
 const questEl = document.getElementById('quest');
 const colorEl = document.getElementById('color');
 const talentsEl = document.getElementById('talents');
+const loyaltyEl = document.getElementById('loyalty');
+
 
 
 const jsonObject = window.localStorage.getItem('application');
@@ -12,3 +14,4 @@ nameEl.textContent = hydratedJson.name;
 questEl.textContent = hydratedJson.quest;
 colorEl.textContent = hydratedJson.color;
 talentsEl.textContent = hydratedJson.talents.join(', ');
+loyaltyEl.textContent = hydratedJson.loyalty;
