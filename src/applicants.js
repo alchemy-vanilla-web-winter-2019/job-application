589a0e062df37a8badb8b0f0b2f57057b0b081a8
mcnadams <mcneilbo@oregonstate.edu>
2019-02-06 15:31:05 -0800

    counted up number of applicants with each cuddle type and display in table

--- src/applicants.js
+++ src/applicants.js
@@ -1,6 +1,7 @@
 const tbody = document.getElementById('applicants');
 const jsonApplicants = window.localStorage.getItem('applicants');
 let applicants = [];
+let quiet = 0, spooning = 0, fire = 0, chatty = 0;
 
 if(jsonApplicants) {
     applicants = JSON.parse(jsonApplicants);
@@ -27,13 +28,57 @@ for(let i = 0; i < applicants.length; i++) {
     }
     else {
         for(let i = 0; i < applicant.cuddles.length - 1; i++) {
-            cuddleTypes.textContent += applicant.cuddles[i] + ', ';
+            const cuddle = applicant.cuddles[i];
+            cuddleTypes.textContent += cuddle + ', ';
+            if(cuddle === 'quiet'){
+                quiet++;
+            }
+            if(cuddle === 'spooning'){
+                spooning++;
+            }
+            if(cuddle === 'chatty'){
+                chatty++;
+            }
+            if(cuddle === 'fire'){
+                fire++;
+            }
         }
-        cuddleTypes.textContent += applicant.cuddles[applicant.cuddles.length - 1];
+        const cuddle = applicant.cuddles[applicant.cuddles.length - 1];
+        cuddleTypes.textContent += cuddle;
+        if(cuddle === 'quiet'){
+            quiet++;
+        }
+        if(cuddle === 'spooning'){
+            spooning++;
+        }
+        if(cuddle === 'chatty'){
+            chatty++;
+        }
+        if(cuddle === 'fire'){
+            fire++;
+        }
     }
-
     tr.appendChild(name);
     tr.appendChild(city);
     tr.appendChild(cuddleTypes);
     tbody.appendChild(tr);
 }
+
+const cuddleTally = document.getElementById('tally-styles');
+const tallyRow = document.createElement('tr');
+const spoonCell = document.createElement('td');
+const fireCell = document.createElement('td');
+const quietCell = document.createElement('td');
+const chattyCell = document.createElement('td');
+
+spoonCell.textContent = spooning;
+fireCell.textContent = fire;
+quietCell.textContent = quiet;
+chattyCell.textContent = chatty;
+
+tallyRow.appendChild(spoonCell);
+tallyRow.appendChild(fireCell);
+tallyRow.appendChild(quietCell);
+tallyRow.appendChild(chattyCell);
+
+cuddleTally.appendChild(tallyRow);
