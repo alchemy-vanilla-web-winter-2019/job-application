eaeeb0f2b676f491fceb8818af47491319280e3e
Leslie Shea <leslieyshea@gmail.com>
2019-02-06 14:50:38 -0800

    linked applicants review page to main page

---
+++ src/applicants-review.js
@@ -0,0 +1,21 @@
+let applicants = [];
+
+const jsonApplicants = window.localStorage.getItem('applicants');
+
+if(jsonApplicants) {
+    applicants = JSON.parse(jsonApplicants);
+}
+else {
+    window.location = './index.html';
+}
+
+const applicantsNode = document.getElementById('applicants');
+
+for(let index = 0; index < applicants.length; index++) {
+    const applicant = applicants[index];
+
+    const row = document.createElement('tr');
+    const firstName = document.createElement('td');
+    const lastName = document.createElement('td');
+
+}
