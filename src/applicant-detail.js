5b9dfb76280739c6fa32e66794876e4f4d25687b
Mallarie <mallariemilano@gmail.com>
2019-02-06 16:22:04 -0800

    fixed a couple typos so the application details would populate

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -3,7 +3,7 @@ const position = document.getElementById('position');
 const foods = document.getElementById('foods');
 
 const json = window.localStorage.getItem('applicant');
-cost foodApplicant = JSON.parse(json);
+const foodApplicant = JSON.parse(json);
 
 name.textContent = foodApplicant.name;
 position.textContent = foodApplicant.position;
