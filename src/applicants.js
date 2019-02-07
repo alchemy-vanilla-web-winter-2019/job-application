b46065127ac2f737d51f8cc81aaf21ab029b6dd4
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-07 09:30:11 -0800

    before the day gets started

--- src/applicants.js
+++ src/applicants.js
@@ -2,15 +2,21 @@ const retrieveJson = window.localStorage.getItem('job-applicant');
 const applicantData = JSON.parse(retrieveJson);
 const tableNode = document.getElementById('table');
 const buttonNode = document.getElementById('return-home');
+let applicantArray = [];
 
-for(let i = 0; i < applicantData.length; i++) {
-    const applicants = applicantData[i];
+if(applicantData) {
+    applicantArray = applicantData;
+}
+
+for(let i = 0; i < applicantArray.length; i++) {
+    const applicants = applicantArray[i];
+    
     const newRow = document.createElement('tr');
 
     const nameEntry = document.createElement('td');
     nameEntry.textContent = applicants.name;
     newRow.appendChild(nameEntry);
-
+    
     const seperatorEntry = document.createElement('td');
     seperatorEntry.textContent = '|';
     newRow.appendChild(seperatorEntry);
