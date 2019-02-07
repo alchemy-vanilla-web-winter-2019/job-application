e9b502ff1d90ddde0bdb54a65737d9fc0f6dd172
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-06 16:36:32 -0800

    Populate table of applicants
    Dynamically adds imporant information from application for each
    application submission

--- src/applicant-list/applicant-list.js
+++ src/applicant-list/applicant-list.js
@@ -1,6 +1,27 @@
 const attributesNode = document.getElementById('attributes');
 
-//request JSON data
-//Do something if it exists,nothing if it doesn't
-//for loop around array of objects
-// --> add elements
+let applicationsArray = [];
+const jsonArrayString = window.localStorage.getItem('applications');
+if(jsonArrayString) {
+    applicationsArray = JSON.parse(jsonArrayString);
+}
+
+for(let i = 0; i < applicationsArray.length; i++) {
+    const newRow = document.createElement('tr');
+    const nameTD = document.createElement('td');
+    const weaponTD = document.createElement('td');
+    const rolesTD = document.createElement('td');
+    const lawfulnessTD = document.createElement('td');
+
+    nameTD.textContent = applicationsArray[i].name;
+    weaponTD.textContent = applicationsArray[i].weapon;
+    rolesTD.textContent = applicationsArray[i].roles.join(', ');
+    lawfulnessTD.textContent = applicationsArray[i].lawfulness;
+
+    newRow.appendChild(nameTD);
+    newRow.appendChild(weaponTD);
+    newRow.appendChild(rolesTD);
+    newRow.appendChild(lawfulnessTD);
+    attributesNode.appendChild(newRow);
+
+}
