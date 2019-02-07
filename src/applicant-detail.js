a375cf1cf9512d4c1f393c0e9acc9db808ce127b
Payton Lower <paytown@me.com>
2019-02-06 16:03:54 -0800

    working build, now on to styling

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -1,12 +1,21 @@
+const json = window.localStorage.getItem('allApplicants');
+
+// const hydrated = JSON.parse(json);
+
+let hydrated = null;
+if(json) {
+    const allApplicants = JSON.parse(json);
+    hydrated = allApplicants[allApplicants.length - 1];
+} else {
+    window.location = '/';
+}
+
 const name = document.getElementById('name');
 const ssn = document.getElementById('ssn');
 const read = document.getElementById('read-answer');
 const shorts = document.getElementById('shorts');
 const favoriteOptions = document.getElementById('favorite-options');
 const hateShortsAmmount = document.getElementById('hate-shorts');
-
-const json = window.localStorage.getItem('applicant');
-const hydrated = JSON.parse(json);
 
 name.textContent = hydrated.name;
 ssn.textContent = hydrated.ssn;
--- src/applicant-review.js
+++ src/applicant-review.js
@@ -5,24 +5,38 @@ if(json) {
     allApplicants = JSON.parse(json);
 }
 
-// let allApplicants = null;
-// if(json) {
-//     allApplicants = JSON.parse(json);
-// } else {
-//     allApplicants = [];
-// }
-
-const tbody = document.getElementById('tbody');
-
 // make elements written in the html
+const tbody = document.getElementById('applicant-review');
+
 for(let i = 0; i < allApplicants.length; i++) {
     const applicant = allApplicants[i];
 
     const tr = document.createElement('tr');
 
+    // render cells
     const nameCell = document.createElement('td');
     nameCell.textContent = applicant.name;
     tr.appendChild(nameCell);
+
+    const ssnCell = document.createElement('td');
+    ssnCell.textContent = applicant.ssn;
+    tr.appendChild(ssnCell);
+
+    const readCell = document.createElement('td');
+    readCell.textContent = applicant.read;
+    tr.appendChild(readCell);
+
+    const shortsCell = document.createElement('td');
+    shortsCell.textContent = applicant.shorts;
+    tr.appendChild(shortsCell);
+
+    const favoriteOptionsCell = document.createElement('td');
+    favoriteOptionsCell.textContent = applicant.favoriteOptions.join(', ');
+    tr.appendChild(favoriteOptionsCell);
+
+    const hateShortsCell = document.createElement('td');
+    hateShortsCell.textContent = applicant.hateShortsAmmount;
+    tr.appendChild(hateShortsCell);
 
     tbody.appendChild(tr);
 }
