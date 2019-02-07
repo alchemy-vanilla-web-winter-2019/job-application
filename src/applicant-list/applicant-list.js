b4d057d02509956a8e16d2c41a37dd123daca757
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-06 17:50:19 -0800

    End of the day

--- src/applicant-list/applicant-list.js
+++ src/applicant-list/applicant-list.js
@@ -4,8 +4,14 @@ const numberOfColumns = document.getElementsByClassName('column');
 const newApplicationsStringArray = window.localStorage.getItem('newApplications');
 const newApplications = JSON.parse(newApplicationsStringArray);
 
+//if skills is empty
+
 for(let i = 0; i < newApplications.length; i++) {
     const newApplicantRow = document.createElement('tr');
+    if(i % 2 !== 0) {
+        newApplicantRow.classList.add('even');
+    }
+    
     const headerArray = [newApplications[i].name, newApplications[i].clothes, newApplications[i].skills.join(', ')];    
     
     for(let j = 0; j < numberOfColumns.length; j++) {
