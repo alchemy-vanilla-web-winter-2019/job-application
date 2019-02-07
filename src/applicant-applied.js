28db9129523bc9483cadb2c76013997c98f1ba11
Colin Coutts <couttstcolin@gmail.com>
2019-02-06 17:33:35 -0800

    Add files via upload

--- src/applicant-applied.js
+++ src/applicant-applied.js
@@ -1,3 +1,15 @@
+const json = window.localStorage.getItem('applicants');
+// double check that this isn't applicant
+let applicant = null;
+
+if(json) {
+    const allApplicants = JSON.parse(json);
+    applicant = allApplicants[allApplicants.length - 1];
+} else {
+    window.location = '/';
+}
+
+
 const name = document.getElementById('name');
 const city = document.getElementById('city');
 const state = document.getElementById('state');
@@ -6,12 +18,11 @@ const weapon = document.getElementById('weapon');
 const slider = document.getElementById('slider');
 
 
-const json = window.localStorage.getItem('applicant');
-const hydratedName = JSON.parse(json);
+// const hydratedName = JSON.parse(json);
 
-name.textContent = hydratedName.name;
-city.textContent = hydratedName.city;
-state.textContent = hydratedName.state;
-pranks.textContent = hydratedName.prank;
-weapon.textContent = hydratedName.weapon.join(', ');
-slider.textContent = hydratedName.slider;
+name.textContent = applicant.name;
+city.textContent = applicant.city;
+state.textContent = applicant.state;
+pranks.textContent = applicant.prank;
+weapon.textContent = applicant.weapon.join(', ');
+slider.textContent = applicant.slider;
