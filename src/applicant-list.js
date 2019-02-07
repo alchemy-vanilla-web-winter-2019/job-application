07b9f3280a9ac12f098c6920b5a1680a0ea1933e
Alexandre Muniz <aom@puzzlezapper.com>
2019-02-06 16:45:55 -0800

    populate table with applicant details

--- src/applicant-list.js
+++ src/applicant-list.js
@@ -1,2 +1,31 @@
 const applicantList = document.getElementById('applicant-list');
-const jsonArray = window.localStorage.getItem('applications');
+const jsonArray = window.localStorage.getItem('applicants');
+let applicants = [];
+if(jsonArray) {
+    applicants = JSON.parse(jsonArray);
+}
+console.log(applicants);
+for(let i = 0; i < applicants.length; i++) {
+    const newRow = document.createElement('tr');
+    applicantList.appendChild(newRow);
+
+    const nameCell = document.createElement('td');
+    const questCell = document.createElement('td');
+    const colorCell = document.createElement('td');
+    const talentsCell = document.createElement('td');
+    const loyaltyCell = document.createElement('td');
+
+    nameCell.textContent = applicants[i].name;
+    questCell.textContent = applicants[i].quest;
+    colorCell.textContent = applicants[i].color;
+    talentsCell.textContent = applicants[i].talents.join(', ');
+    loyaltyCell.textContent = applicants[i].loyalty;
+
+    newRow.appendChild(nameCell);
+    newRow.appendChild(questCell);
+    newRow.appendChild(colorCell);
+    newRow.appendChild(talentsCell);
+    newRow.appendChild(loyaltyCell);
+
+
+}
