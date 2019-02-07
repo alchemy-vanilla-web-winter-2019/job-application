f684a79409590cdcf8a50b9040339c93401b5faa
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-07 14:23:27 -0800

    Use search URL to create individual page per app
    Just like we did in class.

--- src/applicant-detail/applicant-detail.js
+++ src/applicant-detail/applicant-detail.js
@@ -6,15 +6,32 @@ const snuckNode = document.getElementById('snuck');
 
 const snuckTitleNode = document.getElementById('snuck-title');
 
-const applicantSerialized = window.localStorage.getItem('newApplication');
-const applicantParsed = JSON.parse(applicantSerialized);
+const newApplicationsStringArray = window.localStorage.getItem('newApplications');
+let newApplications = [];
+if(newApplicationsStringArray) {
+    newApplications = JSON.parse(newApplicationsStringArray);
+} 
 
-nameNode.textContent = applicantParsed.name;
-assassinationNode.textContent = applicantParsed.assassination;
-clothesNode.textContent = applicantParsed.clothes;
-skillsNode.textContent = applicantParsed.skills.join(', ');
-snuckNode.textContent = applicantParsed.snuck;
 
-const uppercaseFirstLetter = applicantParsed.name.charAt(0).toUpperCase();
-const capitalizedName = uppercaseFirstLetter + applicantParsed.name.slice(1);
+const searchParams = new URLSearchParams(window.location.search);
+const nameToFind = searchParams.get('name');
+console.log(nameToFind);
+console.log(newApplications);
+
+let applicant = {};
+for(let i = 0; i < newApplications.length; i ++) {
+    if(nameToFind === newApplications[i].name) {
+        applicant = newApplications[i];
+        break;
+    }
+}
+
+nameNode.textContent = applicant.name;
+assassinationNode.textContent = applicant.assassination;
+clothesNode.textContent = applicant.clothes;
+skillsNode.textContent = applicant.skills.join(', ');
+snuckNode.textContent = applicant.snuck;
+
+const uppercaseFirstLetter = applicant.name.charAt(0).toUpperCase();
+const capitalizedName = uppercaseFirstLetter + applicant.name.slice(1);
 snuckTitleNode.innerHTML = `Last time ${capitalizedName} snuck up on someone:`;
--- src/applicant-list/applicant-list.js
+++ src/applicant-list/applicant-list.js
@@ -1,5 +1,11 @@
 const newApplicationsStringArray = window.localStorage.getItem('newApplications');
-const newApplications = JSON.parse(newApplicationsStringArray);
+let newApplications = [];
+if(newApplicationsStringArray) {
+    newApplications = JSON.parse(newApplicationsStringArray);
+} else {
+    const noApplications = { none: true };
+    newApplications.push(noApplications);
+}
 
 const tableSection = document.getElementById('table-section');
 const applicantsTable = document.createElement('table');
@@ -9,31 +15,41 @@ tableSection.appendChild(applicantsTable);
 applicantsTable.appendChild(applicantsHeader);
 applicantsTable.appendChild(applicantsBody);
 
-const applicationKeys = Object.keys(newApplications[0]);
-const tableHeadings = [applicationKeys[0], applicationKeys[2], applicationKeys[3]];
-const headingRow = document.createElement('tr');
-applicantsHeader.appendChild(headingRow);
-for(let i = 0; i < tableHeadings.length; i++) {
-    const heading = document.createElement('th');
-    heading.textContent = tableHeadings[i];
-    headingRow.appendChild(heading);
-}
-
-const numberOfColumns = document.getElementsByTagName('th');
-
-for(let i = 0; i < newApplications.length; i++) {
-    const newApplicantRow = document.createElement('tr');
-    if(i % 2 !== 0) {
-        newApplicantRow.classList.add('even');
+if(newApplications[0].none !== true) {
+    const applicationKeys = Object.keys(newApplications[0]);
+    const tableHeadings = [applicationKeys[0], applicationKeys[2], applicationKeys[3]];
+    const headingRow = document.createElement('tr');
+    applicantsHeader.appendChild(headingRow);
+    for(let i = 0; i < tableHeadings.length; i++) {
+        const heading = document.createElement('th');
+        heading.textContent = tableHeadings[i];
+        headingRow.appendChild(heading);
     }
     
-    const headerArray = [newApplications[i].name, newApplications[i].clothes, newApplications[i].skills.join(', ')];    
+    const numberOfColumns = document.getElementsByTagName('th');
     
-    for(let j = 0; j < numberOfColumns.length; j++) {
-        const newTD = document.createElement('td');
-        newTD.textContent = headerArray[j];
+    for(let i = 0; i < newApplications.length; i++) {
+        const newApplicantRow = document.createElement('tr');
+        if(i % 2 !== 0) {
+            newApplicantRow.classList.add('even');
+        }
         
-        newApplicantRow.appendChild(newTD);
+        const headerArray = [newApplications[i].name, newApplications[i].clothes, newApplications[i].skills.join(', ')];    
+        
+        for(let j = 0; j < numberOfColumns.length; j++) {
+            const newTD = document.createElement('td');
+            if(j === 0) {
+                const nameAnchor = document.createElement('a');
+                nameAnchor.href = 'applicant-detail.html?name=' + encodeURIComponent(headerArray[j]);
+                nameAnchor.textContent = headerArray[j];
+                nameAnchor.classList.add('link');
+                newTD.appendChild(nameAnchor);
+            } else {
+                newTD.textContent = headerArray[j];
+            }
+    
+            newApplicantRow.appendChild(newTD);
+        }
+        applicantsBody.appendChild(newApplicantRow);
     }
-    applicantsBody.appendChild(newApplicantRow);
 }
