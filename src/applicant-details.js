d7c980203567e85f5b4698996e38836a5dfbdef2
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-07 12:05:18 -0800

    lunch break

--- src/applicant-details.js
+++ src/applicant-details.js
@@ -1,21 +1,40 @@
 const getPackage = window.localStorage.getItem('job-applicant');
 const applicantArray = JSON.parse(getPackage);
-const lastApp = applicantArray.length - 1;
+const detailsNode = document.getElementById('return-form');
 const buttonNode = document.getElementById('return-home');
-const formValue = [
-    applicantArray[lastApp].name, 
-    applicantArray[lastApp].phone, 
-    applicantArray[lastApp].comfort, 
-    applicantArray[lastApp].burial
+
+const build = [
+    {
+        item: 'Name:',
+        value: 'name'
+    },
+    {
+        item: 'Phone Number:',
+        value: 'phone'
+    },
+    {
+        item: 'Level of Comfort with Immolation', 
+        value: 'comfort'
+    },
+    {
+        item: 'Preferred Burial Place',
+        value: 'burial'
+    }
+ 
+   
 ];
 
-
-for(let index = 0; index < 4; index++) {
-    const form = document.getElementsByTagName('dd');
-    form[index].textContent = formValue[index];
+for(let index = 0; index < build.length; index++) {
+    const applicant = applicantArray[index];
+    const applicant
+    const td = document.createElement('td');
+    const dd = document.createElement('dd');
+    td.textContent = build[index];
+    dd.textContent = applicant[1]
+    console.log('td', td);
+    console.log('dd', dd);
 }
 
 buttonNode.addEventListener('click', function() {
     window.location = './index.html';
 });
-
